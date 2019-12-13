ef54cef596d0b388c17d5be6247edfe87d00da63
utkucanturkan <utkucanturkan@gmail.com>
2019-12-13 18:09:23 +0100

    Procedural texturing implementation

--- include/raytracer.h
+++ include/raytracer.h
@@ -41,11 +41,17 @@ class RayTracer {
                 // TODO Implement this
 
                 glm::dvec2 screenCoord((2.0f * x) / w - 1.0f, (-2.0f * y) / h + 1.0f);
-                Ray ray(_camera.pos,
-                        (forward + screenCoord.x * _w * right + screenCoord.y * _h * up));
+                Ray ray(_camera.pos, (forward + screenCoord.x * _w * right + screenCoord.y * _h * up));
+
+				//float dir_x = (y + 0.5) - w / 2.;
+    //            float dir_y = -(x + 0.5) + h / 2.; 
+    //            float dir_z = -h / (2. * tan(M_PI / 3. / 2.));
+
+				//glm::dvec3 dir(dir_x, dir_y, dir_z);
+				//Ray ray(glm::dvec3(0,0,0), dir);
 
                 float tnear = INFINITY;
-                glm::dvec3 color;
+                glm::dvec3 color(0.1, 0.5, 0.6);
                 for (auto& e : _scene->intersect(ray)) {
                     float t0 = INFINITY;
                     glm::dvec3 intersection{t0, 0, 0};
@@ -60,19 +66,16 @@ class RayTracer {
                             normal = glm::normalize(intersection - e->pos);
 
                             glm::dvec3 reflect_dir = glm::normalize(reflect(ray.dir, normal));
-                            glm::dvec3 refract_dir = glm::normalize(refract(ray.dir, normal, e->material.refractive_index));
+                            glm::dvec3 refract_dir = glm::normalize(
+                                refract(ray.dir, normal, e->material.refractive_index));
 
                             glm::dvec3 reflect_orig = glm::dot(reflect_dir, normal) < 0
-															? intersection - normal * 1e-3
-															: intersection + normal * 1e-3;
-
-                                                        
-                            glm::dvec3 refract_orig = glm::dot(reflect_dir, normal) < 0
                                                           ? intersection - normal * 1e-3
                                                           : intersection + normal * 1e-3;
 
-                            //glm::dvec3 reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
-                            //glm::dvec3 refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
+                            glm::dvec3 refract_orig = glm::dot(reflect_dir, normal) < 0
+                                                          ? intersection - normal * 1e-3
+                                                          : intersection + normal * 1e-3;
 
                             float lightIntensity = 1.0, diffuse_light_intensity = 0, specular_light_intensity = 0;
 
@@ -86,25 +89,41 @@ class RayTracer {
                             tempColor.operator*=(e->material.albedo[0]);
 
                             tempColor = tempColor + glm::dvec3(1.0, 1.0, 1.0).operator*=(specular_light_intensity).operator*=(e->material.albedo[1]);
-                            //tempColor = tempColor + reflect_color.operator*=(e->material.albedo.[2]);
-                            //tempColor = tempColor + refract_color.operator*=(e->material.albedo[3]);
                             color = tempColor;
                         }
-                    }
+                    } else {
+                        float checkerboard_dist = std::numeric_limits<float>::max();
+                        if (fabs(ray.dir[1]) > 1e-3) {
+                            float d = -(ray.origin[1] + 1) / ray.dir[1]; // the checkerboard plane has equation x = -5
+                            glm::dvec3 tempRayDir = ray.dir;
+                            glm::dvec3 pt = ray.origin + tempRayDir.operator*=(d);
+                            if (d > 0 && fabs(pt[0]) < 10 && pt[2] < 5 && pt[2] > -30 &&
+                                d < tnear) {
+                                checkerboard_dist = d;
+                                intersection = pt;
+                                normal = glm::dvec3(0, 1, 0);
+                                color = (int(.5 * intersection[0] + 1000) + int(.5 * intersection[2])) & 1 
+									? glm::dvec3(1, .6, .1) 
+									: glm::dvec3(1, 1, 1);
+                            }
+                        }
+					}
                 }
                 _image->setPixel(x, y, color);
             }
         }
     }
 
-	glm::dvec3 refract(const glm::dvec3& I, const glm::dvec3& N, const float eta_t, const float eta_i = 1.f) { // Snell's law
+    glm::dvec3 refract(const glm::dvec3& I, const glm::dvec3& N, const float eta_t, const float eta_i = 1.f) { 
+		// Snell's law
         float cosi = -std::max(-1.0, std::min(1.0, glm::dot(I, N)));
 
-        if (cosi < 0) return refract(I, -N, eta_i, eta_t);
+        if (cosi < 0)
+            return refract(I, -N, eta_i, eta_t);
         float eta = eta_i / eta_t;
         float k = 1 - eta * eta * (1 - cosi * cosi);
 
-		glm::dvec3 tempI = I;
+        glm::dvec3 tempI = I;
         glm::dvec3 tempN = N;
 
         return k < 0 ? glm::dvec3(1, 0, 0) : tempI.operator*=(eta) + tempN.operator*=((eta * cosi - sqrtf(k)));
--- main.cpp
+++ main.cpp
@@ -11,27 +11,25 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
     // x, y, z
-    Camera camera({0, 0, 20});
+    Camera camera({0, 5, 15});
 
-    glm::dvec3 light{0, 0, 10};
+    glm::dvec3 light{0, 10, 10};
 
     RayTracer raytracer(camera, light);
 
     // Materials
     Material ivory(glm::dvec3(0.4, 0.4, 0.3), 1.0, glm::dvec4(0.6, 0.3, 0.1, 0.0), 50.);
-    Material glass(glm::dvec3(0.6, 0.7, 0.8), 1.5, glm::dvec4(0.0, 0.5, 0.1, 0.8), 125.);
+    Material glass(glm::dvec3(0.6, 0.7, 0.8), 1.5, glm::dvec4(0.6, 0.3, 0.1, 0.0), 125.);
     Material red_rubber(glm::dvec3(1, 0, 0), 1.0, glm::dvec4(0.9, 0.1, 0.0, 0.0), 10.);
-    Material mirror(glm::dvec3(1.0, 1.0, 1.0), 1.0, glm::dvec4(0.0, 10.0, 0.8, 0.0), 1425.);
+    Material mirror(glm::dvec3(1.0, 1.0, 1.0), 1.0, glm::dvec4(0.6, 0.3, 0.1, 0.0), 1425.);
 
     // x, y, z
-    Sphere* ivorySphere = &Sphere({-3, 0, -6}, 2, ivory);
-    Sphere* glassSphere = &Sphere({0, 3, -6}, 2, glass);
-    Sphere* redRubberSphere = &Sphere({3, 0, -6}, 2, red_rubber);
-    Sphere* mirrorSphere = &Sphere({0, -3, -6}, 2, mirror);
+    Sphere* ivorySphere = &Sphere({-2, 3, -5}, 2, ivory);
+    Sphere* glassSphere = &Sphere({-3, 1, 0}, 2, glass);
+    Sphere* redRubberSphere = &Sphere({2, 1, 0}, 2, red_rubber);
+    Sphere* mirrorSphere = &Sphere({2, 4, -5}, 2, mirror);
 
-    // Triangle* bluetriangle = &Triangle({0, 0, -5}, {2, 0, -5}, {2, 2, -5}, Material({1, 0, 0}));
-    // Triangle* redtriangle = &Triangle({-5, -1, -1}, {-5, -1, 1}, {-5, 1, 0}, Material({1, 0,
-    // 0}));
+    Triangle* triangle = &Triangle({0, 0, -10}, {1, 0, -10}, {1, 1, -10}, ivory);
 
     // Set up scene
     Octree scene({-20, -20, -20}, {20, 20, 20});
@@ -42,8 +40,7 @@ int main(int argc, char** argv) {
     scene.push_back(glassSphere);
     scene.push_back(mirrorSphere);
 
-    // scene.push_back(bluetriangle);
-    // scene.push_back(redtriangle);
+    //scene.push_back(triangle);
 
     raytracer.setScene(&scene);
 
