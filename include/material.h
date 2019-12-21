cc9d5119728a45cd2a66832c88a1cbff1a81461f
utkucanturkan <utkucanturkan@gmail.com>
2019-12-21 13:25:10 +0100

    Reflect and refract implementation

--- include/material.h
+++ include/material.h
@@ -5,14 +5,9 @@
 /// Represents the material properties of an entity. For now it only contains color, but it should
 /// probably be extended to allow more options.
 struct Material {
-    constexpr explicit Material(glm::dvec3 color,
-                                const float refractiveIndex,
-                                const glm::dvec4& albedo,
-                                const float specularExponent)
-        : color(std::move(color)), refractive_index(refractiveIndex), albedo(albedo),
-          specular_exponent(specularExponent) {}
+    constexpr explicit Material(glm::dvec3 color,const float refractiveIndex,const glm::dvec4& albedo,const float specularExponent): color(std::move(color)), refractive_index(refractiveIndex), albedo(albedo), specular_exponent(specularExponent) {}
 
-	Material() {}
+    Material() : refractive_index(1), albedo(1, 0, 0, 0), color(), specular_exponent() {}
 
     glm::dvec3 color;
     float refractive_index;
--- include/raytracer.h
+++ include/raytracer.h
@@ -24,7 +24,6 @@ class RayTracer {
     void setScene(const Octree* scene) { _scene = scene; }
 
     void run(int w, int h) {
-        // TODO Implement this
         _image = std::make_shared<Image>(w, h);
 
         glm::dvec3 forward = _camera.forward;
@@ -38,121 +37,19 @@ class RayTracer {
         // The structure of the for loop should remain for incremental rendering.
         for (int y = 0; y < h && _running; ++y) {
             for (int x = 0; x < w && _running; ++x) {
-                // TODO Implement this
 
                 glm::dvec2 screenCoord((2.0f * x) / w - 1.0f, (-2.0f * y) / h + 1.0f);
                 Ray ray(_camera.pos,
                         (forward + screenCoord.x * _w * right + screenCoord.y * _h * up));
 
-                // float dir_x = (y + 0.5) - w / 2.;
-                // float dir_y = -(x + 0.5) + h / 2.;
-                // float dir_z = -h / (2. * tan(M_PI / 3. / 2.));
+                glm::dvec3 pixelColor = traceRay(ray);
 
-                // glm::dvec3 dir(dir_x, dir_y, dir_z);
-                // Ray ray(glm::dvec3(0,0,0), dir);
-
-                float tnear = INFINITY;
-                glm::dvec3 color(0.1, 0.5, 0.6);
-                for (auto& e : _scene->intersect(ray)) {
-                    float t0 = INFINITY;
-                    glm::dvec3 intersection{t0, 0, 0};
-                    glm::dvec3 normal;
-
-                    if (e->intersect(ray, intersection, normal)) {
-                        if (intersection[0] < tnear) {
-                            tnear = intersection[0];
-
-                            glm::dvec3 tempRayDir = ray.dir;
-                            intersection = ray.origin + tempRayDir.operator*=(tnear);
-                            normal = glm::normalize(intersection - e->pos);
-
-                            glm::dvec3 reflect_dir = glm::normalize(reflect(ray.dir, normal));
-                            glm::dvec3 refract_dir = glm::normalize(
-                                refract(ray.dir, normal, e->material.refractive_index));
-
-                            glm::dvec3 reflect_orig = glm::dot(reflect_dir, normal) < 0
-                                                          ? intersection - normal * 1e-3
-                                                          : intersection + normal * 1e-3;
-
-                            glm::dvec3 refract_orig = glm::dot(reflect_dir, normal) < 0
-                                                          ? intersection - normal * 1e-3
-                                                          : intersection + normal * 1e-3;
-
-                            float lightIntensity = 1.0, diffuse_light_intensity = 0,
-                                  specular_light_intensity = 0;
-
-                            glm::dvec3 light_dir = glm::normalize((_light - intersection));
-                            float light_distance = glm::length(_light - intersection);
-
-                            // SHADOW
-                            glm::dvec3 shadowOrigin = (glm::dot(light_dir, normal) < 0)
-                                                          ? intersection - normal * 1e-3
-                                                          : intersection + normal * 1e-3;
-
-                            float shadowTNear = INFINITY;
-                            bool isShadow = false;
-                            float shadowT0;
-                            glm::dvec3 shadowIntersection, shadowNormal;
-                            for (auto& e : _scene->intersect(ray)) {
-                                shadowT0 = INFINITY;
-                                shadowIntersection = {shadowT0, 0, 0};
-                                Ray shadowRay(shadowOrigin, light_dir);
-                                if (e->intersect(shadowRay, shadowIntersection, shadowNormal)) {
-                                    if (shadowIntersection[0] < tnear) {
-                                        shadowTNear = shadowIntersection[0];
-                                        glm::dvec3 tempShadowDir = shadowRay.dir;
-                                        shadowIntersection = shadowRay.origin +
-                                                             tempShadowDir.operator*=(shadowTNear);
-                                    }
-                                }
-                            }
-
-                            if (glm::length((shadowIntersection - shadowOrigin)) < light_distance) {
-                                isShadow = true;
-                            } else {
-                                isShadow = false;
-                            }
-
-                            if (!isShadow) {
-                                diffuse_light_intensity += lightIntensity * std::max(0.0, glm::dot(light_dir, normal));
-                                specular_light_intensity += powf(std::max(0.0, glm::dot(-reflect(-light_dir, normal), ray.dir)), e->material.specular_exponent) * lightIntensity;
-                            }
-
-                            glm::dvec3 tempColor = e->material.color;
-                            tempColor.operator*=(diffuse_light_intensity);
-                            tempColor.operator*=(e->material.albedo[0]);
-                            tempColor = tempColor + glm::dvec3(1.0, 1.0, 1.0).operator*=(specular_light_intensity).operator*=(e->material.albedo[1]);
-                            color = tempColor;
-                        }
-                    } else {
-						// CHECKERBOARD
-                        float checkerboard_dist = std::numeric_limits<float>::max();
-                        if (fabs(ray.dir[1]) > 1e-3) {
-                            float d = -(ray.origin[1] + 1) /
-                                      ray.dir[1]; // the checkerboard plane has equation x = -5
-                            glm::dvec3 tempRayDir = ray.dir;
-                            glm::dvec3 pt = ray.origin + tempRayDir.operator*=(d);
-                            if (d > 0 && fabs(pt[0]) < 10 && pt[2] < 5 && pt[2] > -30 &&
-                                d < tnear) {
-                                checkerboard_dist = d;
-                                intersection = pt;
-                                normal = glm::dvec3(0, 1, 0);
-                                color =
-                                    (int(.5 * intersection[0] + 1000) + int(.5 * intersection[2])) &
-                                            1
-                                        ? glm::dvec3(1, .6, .1)
-                                        : glm::dvec3(1, 1, 1);
-                            }
-                        }
-                    }
-                }
-                _image->setPixel(x, y, color);
+                _image->setPixel(x, y, pixelColor);
             }
         }
     }
 
-    glm::dvec3
-    refract(const glm::dvec3& I, const glm::dvec3& N, const float eta_t, const float eta_i = 1.f) {
+    glm::dvec3 refract(const glm::dvec3& I, const glm::dvec3& N, const float eta_t, const float eta_i = 1.f) {
         // Snell's law
         float cosi = -std::max(-1.0, std::min(1.0, glm::dot(I, N)));
 
@@ -166,6 +63,76 @@ class RayTracer {
 
         return k < 0 ? glm::dvec3(1, 0, 0)
                      : tempI.operator*=(eta) + tempN.operator*=((eta * cosi - sqrtf(k)));
+    }
+
+    bool sceneIntersect(const Ray& ray, glm::dvec3& hitPoint, glm::dvec3& hitNormal, Material& material) {
+        float spheres_dist = INFINITY;
+        for (auto& e : _scene->intersect(ray)) {
+            glm::dvec3 intersection{0, 0, 0};
+            glm::dvec3 normal;
+            if (e->intersect(ray, intersection, normal) && (intersection[0] < spheres_dist)) {
+                glm::dvec3 tempRaydir = ray.dir;
+                spheres_dist = intersection[0];
+                hitPoint = ray.origin + tempRaydir.operator*=(intersection[0]);
+                hitNormal = glm::normalize(hitPoint - e->pos);
+                material = e->material;
+            }
+        }
+
+        // CHECKBOARD
+        float checkerboard_dist = std::numeric_limits<float>::max();
+        if (fabs(ray.dir[1]) > 1e-3) {
+            float d = -(ray.origin[1] + 1) / ray.dir[1]; // the checkerboard plane has equation x = -5
+            glm::dvec3 tempRayDir = ray.dir;
+            glm::dvec3 pt = ray.origin + tempRayDir.operator*=(d);
+            if (d > 0 && fabs(pt[0]) < 10 && pt[2] < 5 && pt[2] > -30 && d < spheres_dist) {
+                checkerboard_dist = d;
+                hitPoint = pt;
+                hitNormal = glm::dvec3(0, 1, 0);
+                material.color = (int(.5 * hitPoint[0] + 1000) + int(.5 * hitPoint[2])) & 1 ? glm::dvec3(1, .6, .1) : glm::dvec3(1, 1, 1);
+            }
+        }
+        return std::min(spheres_dist, checkerboard_dist) < 1000;
+    }
+
+    glm::dvec3 traceRay(const Ray& ray, size_t depth = 0) {
+        glm::dvec3 hit, normal;
+        Material material;
+
+        if (depth > 4 || !sceneIntersect(ray, hit, normal, material)) {
+            return glm::dvec3(0.1, 0.5, 0.6);
+        }
+
+        glm::dvec3 reflect_dir = glm::normalize(reflect(ray.dir, normal));
+        glm::dvec3 refract_dir = glm::normalize(refract(ray.dir, normal, material.refractive_index));
+
+        glm::dvec3 reflect_orig = glm::dot(reflect_dir, normal) < 0 ? hit - normal * 1e-3 : hit + normal * 1e-3;
+        glm::dvec3 refract_orig = glm::dot(refract_dir, normal) < 0 ? hit - normal * 1e-3 : hit + normal * 1e-3;
+
+        glm::dvec3 reflect_color = traceRay(Ray(reflect_orig, reflect_dir), depth + 1);
+        glm::dvec3 refract_color = traceRay(Ray(refract_orig, refract_dir), depth + 1);
+
+        float lightIntensity = 1.0, diffuse_light_intensity = 0, specular_light_intensity = 0;
+
+        glm::dvec3 light_dir = glm::normalize((_light - hit));
+        float light_distance = glm::length(_light - hit);
+
+        glm::dvec3 shadow_orig = glm::dot(light_dir, normal) < 0 ? hit - normal * 1e-3 : hit + normal * 1e-3;
+        glm::dvec3 shadow_pt, shadow_N;
+        Material tmpmaterial;
+
+        if (!sceneIntersect(Ray(shadow_orig, light_dir), shadow_pt, shadow_N, tmpmaterial) && glm::length(shadow_pt - shadow_orig) > light_distance) {
+            return material.color;
+        }
+
+        diffuse_light_intensity += lightIntensity * std::max(0.0, glm::dot(light_dir, normal));
+        specular_light_intensity += powf(std::max(0.0, glm::dot(-glm::reflect(-light_dir, normal), ray.dir)), material.specular_exponent) * lightIntensity;        
+
+        material.color.operator*=(diffuse_light_intensity).operator*=(material.albedo[0]);
+        material.color += glm::dvec3(1., 1., 1.).operator*=(specular_light_intensity).operator*=(material.albedo[1]);
+        material.color += reflect_color.operator*=(material.albedo[2]);
+        material.color += refract_color.operator*=(material.albedo[3]);
+        return material.color;
     }
 
     bool running() const { return _running; }
